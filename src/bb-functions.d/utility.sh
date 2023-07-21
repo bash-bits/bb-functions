1a066168ff86e0cb552935572f7e2b5bd4211f8f
Ragdata <ragdata@users.noreply.github.com>
2023-07-21 18:24:03 +1000

    feat(functions): add utility functions

--- src/bb-functions.d/utility.sh
+++ src/bb-functions.d/utility.sh
@@ -11,18 +11,49 @@
 # License:      MIT License
 # Copyright:    Copyright © 2023 Darren (Ragdata) Poulton
 # ==================================================================
+# DEPENDENCIES
+# ==================================================================
+bb-import bb-functions/is
+# ==================================================================
 # FUNCTIONS
 # ==================================================================
 # ------------------------------------------------------------------
+# utility::bashInit
+# ------------------------------------------------------------------
+# @description Reset terminal session without closing terminal window
+# ------------------------------------------------------------------
+utility::bashInit() { reset; exec sudo --login --user "$USER" /bin/bash -c "cd '$PWD'; exec '$SHELL' -l"; }
+# ------------------------------------------------------------------
 # utility::checkBash
+# ------------------------------------------------------------------
 # ------------------------------------------------------------------
 utility::checkBash() { [[ "${BASH_VERSION:0:1}" -lt 4 ]] && errorExit "This script requires a minimum Bash version of 4+"; }
 # ------------------------------------------------------------------
+# utility::checkCMD
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+utility::checkCMD()
+{
+    [[ $# -eq 0 ]] && return
+    [[ ! $(is::array "$1") ]] && errorReturn "'$1' Not an Array!" 1
+
+    local -n TOOLS="$1"
+    for i in "${!TOOLS[@]}"
+    do
+        if ! command -v "${TOOLS[$i]}" &> /dev/null; then
+            errorReturn "ERROR :: Command '${TOOLS[$i]}' Not Found!" 1
+        fi
+    done
+}
+# ------------------------------------------------------------------
 # utility::checkRoot
+# ------------------------------------------------------------------
 # ------------------------------------------------------------------
 utility::checkRoot() { [[ "$EUID" -ne 0 ]] && errorExit "This script MUST be run as root!"; }
 # ==================================================================
 # ALIAS FUNCTIONS
 # ==================================================================
+bashInit() { utility::bashInit; }
 checkBash() { utility::checkBash; }
 checkRoot() { utility::checkRoot; }
+checkCMD() { utility::checkCMD "$@"; }
