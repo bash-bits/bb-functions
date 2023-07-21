4835a45d256b5a7a2ded70304f211c7718d21f54
Ragdata <ragdata@users.noreply.github.com>
2023-07-21 16:55:36 +1000

    feat(functions): add version function

--- src/bb-functions.sh
+++ src/bb-functions.sh
@@ -15,6 +15,15 @@
 # ==================================================================
 bb-import bb-functions/is
 # ==================================================================
+# VARIABLES
+# ==================================================================
+#
+# BUILD VARIABLES
+#
+declare -gx FUNCTIONS_BUILD="x"
+declare -gx FUNCTIONS_VERSION="v-1.0.0"
+declare -gx FUNCTIONS_BUILD_DATE="20230718-0033"
+# ==================================================================
 # FUNCTIONS
 # ==================================================================
 # ------------------------------------------------------------------
@@ -41,10 +50,58 @@ functions::load()
         bb-import "bb-functions/$file"
     done
 }
+# ------------------------------------------------------------------
+# functions::version
+# ------------------------------------------------------------------
+# @description Reports the version and build date of this release
+#
+# @noargs
+#
+# @stdout Version, Copyright, & Build Information
+# ------------------------------------------------------------------
+functions::version()
+{
+	local verbosity="${1:-}"
+
+	if [[ -z "$verbosity" ]]; then
+		echo "${FUNCTIONS_VERSION}"
+	else
+		echo
+		echo "Bash-Bits Modular Bash Library"
+		echoWhite "BB-Functions Module ${FUNCTIONS_VERSION}"
+		echo "Copyright © 2022-2023 Darren (Ragdata) Poulton"
+		echo "Build: ${FUNCTIONS_BUILD}"
+		echo "Build Date: ${FUNCTIONS_BUILD_DATE}"
+		echo
+	fi
+}
 # ==================================================================
 # MAIN
 # ==================================================================
 # IF SOURCED, RUN LOAD FUNCTION
 if [[ $(is::sourced) ]]; then
 	functions::load "$@" || return $?
+else
+	trap 'bb::errorHandler "LINENO" "BASH_LINENO" "${BASH_COMMAND}" "${?}"' ERR
+	options=$(getopt -l "version::" -o "v::" -a -- "$@")
+
+	evalset --"$options"
+
+	while true
+	do
+		case "$1" in
+			-v|--version)
+				[[ -n "${2}" ]] && { functions::version "${2}"; shift 2; } || { functions::version; shift; }
+				exitReturn 0
+				;;
+			--)
+				shift
+				break
+				;;
+			*)
+				echoError "Invalid Argument!"
+				exitReturn 2
+				;;
+		esac
+	done
 fi
